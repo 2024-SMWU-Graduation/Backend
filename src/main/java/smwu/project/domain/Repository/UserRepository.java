75a350ef08263ee65c86d7185b5cf0d3e09a9c24
tichall <0817frog@gmail.com>
2024-11-05 19:23:45 +0900

    chore : #1 application.yml 수정 - 환경변수 설정

--- src/main/java/smwu/project/domain/Repository/UserRepository.java
+++ src/main/java/smwu/project/domain/Repository/UserRepository.java
@@ -3,5 +3,5 @@ package smwu.project.domain.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import smwu.project.domain.entity.User;
 
-public interface UserRepository extends JpaRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
