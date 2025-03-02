29fa7ac265eedc1cf5dc41d971461fb78aba8348
tichall <0817frog@gmail.com>
2025-03-02 16:39:30 +0900

    fix #33 : import 삭제

--- src/main/java/smwu/project/domain/repository/RandomFeedbackRepository.java
+++ src/main/java/smwu/project/domain/repository/RandomFeedbackRepository.java
@@ -1,8 +1,6 @@
 package smwu.project.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import smwu.project.domain.entity.RandomFeedback;
 import smwu.project.domain.entity.RandomQuestion;
 import smwu.project.global.exception.CustomException;
